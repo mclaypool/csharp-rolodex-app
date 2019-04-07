40f4210614f1d255928cd60ba9ef4d5af33ed631
Max Claypool <mclaypool@users.noreply.github.com>
2019-04-07 16:52:53 -0700

    initial commit

---
+++ RolodexApp/RolodexApp/App_Code/ErrorHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace RolodexApp
+{
+    internal static class ErrorHandler
+    {
+        public static void LogError(Exception e)
+        {
+            string path = "logs\\";
+            string fileName = "Errors_" + DateTime.Now.ToString("yyyyMMddHH") + ".txt";
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path, fileName);
+            System.Diagnostics.Debug.WriteLine(filePath);
+
+            using (var stream = new StreamWriter(filePath, true))
+            {
+                stream.WriteLine(e);
+                stream.WriteLine(e.StackTrace);
+                stream.WriteLine();
+            }
+        }
+    }
+}
---
+++ RolodexApp/RolodexApp/Appointment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RolodexApp
+{
+    public class Appointment
+    {
+        public string Type { get; set; }
+        public DateTime Time { get; set; }
+
+        public Appointment() { Type = "Not Set";  }
+    }
+}
